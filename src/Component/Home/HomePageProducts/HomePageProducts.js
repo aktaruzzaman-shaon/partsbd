b9800a07ca40861cd691ec239d9de0891df20ef0
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2023-04-04 00:13:51 +0600

    added pagination button

--- src/Component/Home/HomePageProducts/HomePageProducts.js
+++ src/Component/Home/HomePageProducts/HomePageProducts.js
@@ -4,24 +4,43 @@ import HomePageProduct from './HomePageProduct';
 const HomePageProducts = () => {
 
     const [homePageProducts, sethomePageProducts] = useState([]);
+    const [pageCount, setPageCount] = useState(0);
+    const [page, setPage] = useState(0);
 
     //loading sample data on home page
     useEffect(() => {
-        fetch('http://localhost:5000/parts')
+        fetch('http://localhost:5000/productCount')
             .then(response => response.json())
-            .then(products => sethomePageProducts(products));
+            .then(data => {
+                const count = data.count;
+                const pages = Math.ceil(count / 6);
+                setPageCount(pages);
+            });
     }, [])
-
 
     return (
         <div >
             <div><p>Products</p></div>
-            <div className='mx-10 grid grid-cols-3 gap-3'>{
-                homePageProducts.map(homePageProduct => <HomePageProduct
-                    homePageProduct={homePageProduct}
-                ></HomePageProduct>
-                )
-            }</div>
+            <div className='mx-10 grid grid-cols-3 gap-3'>
+                {
+                    homePageProducts.map(homePageProduct => <HomePageProduct
+                        homePageProduct={homePageProduct}
+                    ></HomePageProduct>
+                    )
+                }
+
+                {/* pagination button */}
+                <div className="btn-group">
+                    {
+                        [...Array(pageCount).keys()].map(number => <button
+                            className={page === number ? 'btn-active' : ''}
+                            onClick={() => setPage(number)}
+                            >{number + 1}</button>)
+                    }
+                </div>
+            </div>
+
+            {/* See all product */}
             <div className='my-3'><p>See all ...</p></div>
         </div>
     );
--- src/Component/Products/Products.js
+++ src/Component/Products/Products.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from './Product';
 
 const Products = () => {
+    const [allProducts, setallProducts] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/allProducts/')
+            .then(response => response.json())
+            .then(data => setallProducts(data))
+    }, [])
     return (
         <div>
-            <h2>THis is prodcuts page</h2>
+            <h2>Products</h2>
+            <div >
+                <div className='mx-10 grid grid-cols-3 gap-3'>{
+                    allProducts.map(singleProduct => <Product
+                        singleProduct={singleProduct}
+                    ></Product>
+                    )
+                }</div>
+            </div>
         </div>
     );
 };
