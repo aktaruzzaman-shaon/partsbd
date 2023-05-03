e5d21b327d5cd626184c2917ef456fbc382e56fd
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2023-05-03 11:06:12 +0600

    Order page delivery information field added

--- src/Component/OrderPage/OrderPage.js
+++ src/Component/OrderPage/OrderPage.js
@@ -7,11 +7,12 @@ const OrderPage = ({ singleProduct }) => {
     return (
         <div>
             <input type="checkbox" id="edit" className="modal-toggle" />
-            <div className="modal modal-bottom sm:modal-middle">
-                <div className="modal-box">
+            <div className="modal">
+                <div className="modal-box w-11/12 max-w-5xl">
                     <label htmlFor="edit" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <div className="flex">
-                        <div className='m-5'>
+                    
+                    <div className="flex justify-center">
+                        <div className='p-5 mr-5'>
                             <div className="card w-50 bg-base-100 shadow-xl">
                                 <div className="card-body">
                                     <h2 className="card-title">{name}</h2>
@@ -20,14 +21,19 @@ const OrderPage = ({ singleProduct }) => {
                                 </div>
                             </div>
                         </div>
-                        <div>
-                            <p>Delivery Information</p>
+
+                        <div className='ml-5'>
                             <form>
-                                <input type="text" value="shaon" />
+                                <p className='font-extrabold'>Delivery Information</p>
+                                <div className='flex flex-col'>
+                                    <input type="text" placeholder="Name" className="input mb-2 input-bordered w-full max-w-xs" />
+                                    <input type="text" placeholder="Address" className="input mb-2 input-bordered w-full max-w-xs" />
+                                    <input type="text" placeholder="Phone Number" className="input mb-2 input-bordered w-full max-w-xs" />
+                                    <button className="btn">Payment</button>
+                                </div>
                             </form>
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
