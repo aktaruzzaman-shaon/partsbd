ff508b3fed64e9351ffb4bb799fd67ca0a8df4a7
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2023-05-15 19:30:27 +0600

    Added delivery inforamtion update option

--- src/Component/DeliveryInfoPage/DeliveryInfo.js
+++ src/Component/DeliveryInfoPage/DeliveryInfo.js
@@ -1,19 +1,56 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { useForm } from 'react-hook-form';
 
 const DeliveryInfo = () => {
+
+    const [user] = useAuthState(auth);
+    const userEmail = user?.email;
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+    const onSubmit = data => {
+
+        const userDeliveryInfo = { name: data?.Name, phone: data?.PhoneNumber, Address: data?.Address };
+        fetch(`http://localhost:5000/addDeliveryInfo/${userEmail}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(userDeliveryInfo)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                reset()
+            })
+    };
+
+
+
     return (
         <div>
-            <p className='font-extrabold'>Delivery Information</p>
-            <div className='justify-items-center'>
-                <form>
-                    <div className='flex flex-col'>
-                        <input type="text" placeholder="Name" className="input mb-2 input-bordered w-full max-w-xs" />
-                        <input type="text" placeholder="Address" className="input mb-2 input-bordered w-full max-w-xs" />
-                        <input type="text" placeholder="Phone Number" className="input mb-2 input-bordered w-full max-w-xs" />
-                        <input type="submit" value="Submit" className="btn w-24" />
-                    </div>
-                </form>
-            </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className='form-control w-full'>
+                    <label className="label">
+                        <span className="label-text">Name</span>
+                    </label>
+                    <input {...register("Name", { required: true })} className='input input-bordered' />
+                </div>
+
+                <div className="form-control w-full">
+                    <label className="label">
+                        <span className="label-text">PhoneNumber</span>
+                    </label>
+                    <input type='text' {...register("PhoneNumber", { required: true })} className='input input-bordered' />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Address</span>
+                    </label>
+                    <input type='text' {...register("Address", { required: true })} className='input input-bordered' />
+                </div>
+                <input type="submit" className='btn' value="Submit" />
+            </form>
         </div>
     );
 };
