7eb4ba8ced9283f73809c734c80b53373d1d8c6c
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2023-05-21 19:19:24 +0600

    Added product search option

--- src/Component/Products/Products.js
+++ src/Component/Products/Products.js
@@ -8,21 +8,33 @@ import { useQuery } from 'react-query';
 const Products = () => {
 
     const [allProducts, setallProducts] = useState([]);
+    const [searchProductName, setsearchProductName] = useState("");
 
 
     //for loading all data
     const { data, isLoading, refetch } = useQuery('allproducts', () => fetch('http://localhost:5000/allProducts').then(response => response.json()).then(data => setallProducts(data)))
 
     //for loading searching single data
-    const { data: searchProducts, isLoading: searchLoading } = useQuery('allproducts', () => fetch(`http://localhost:5000/allProducts/${name}`).then(response => response.json()).then(data => setallProducts(data)))
 
 
-    console.log(allProducts);
+    const handleSearchName = () => {
+        // const { data: searchProducts, isLoading: searchLoading } = useQuery('allproducts', () => fetch(`http://localhost:5000/allProducts/${searchProductName}`).then(response => response.json()).then(data => setallProducts(data)))
+
+        fetch(`http://localhost:5000/allProducts/${searchProductName}`).then(response => response.json()).then(data => setallProducts(data))
+
+
+    }
+
+
+
     return (
         <div>
             <h2>Products</h2>
             <div>
-                <input type="text" placeholder="search by name" className="input input-bordered w-full max-w-lg mb-5" />
+                <form>
+                    <input type="text" placeholder="search by name" onChange={(e) => setsearchProductName(e.target.value)} className="input input-bordered w-full max-w-lg mb-5" />
+                </form>
+                <button className='btn' onClick={handleSearchName}>Search</button>
             </div>
             <div >
                 <div className='mx-10 grid grid-cols-3 gap-3'>
